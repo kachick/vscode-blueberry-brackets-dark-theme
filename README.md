bcd176cb71f550b2eb860ad21bdf47e0384de493
Kenichi Kamiya <kachick1@gmail.com>
2024-08-14 15:35:18 +0900

    Refine devenv
    Imported from
    
    https://github.com/kachick/anylang-template
    https://github.com/kachick/vscode-extension-yamlfmt

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Blueberry Brackets Dark Theme
+
 Based on [Blueberry Dark Theme](https://github.com/peymanslh/vscode-blueberry-dark-theme) with a little change for brackets.
 
 ![Colors](https://raw.githubusercontent.com/peymanslh/vscode-blueberry-dark-theme/master/colors.jpg)
 
-### Installation
+## Installation
+
 Launch VS Code Quick Open (⌘+P), paste the following command, and press enter.
-```
+
+```plaintext
 ext install blueberry-dark-theme
 ```
 
-### Screen Shot
+## Screen Shot
+
 ![Screen Shot](https://raw.githubusercontent.com/peymanslh/vscode-blueberry-dark-theme/master/screenshot.png)
 
 > The font in the screenshot is [Operator Mono](https://www.typography.com/blog/introducing-operator) .
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default =
+            with pkgs;
+            mkShellNoCC {
+              buildInputs = [
+                bashInteractive
+                findutils # xargs
+                nixfmt-rfc-style
+                nil
+
+                dprint
+                typos
+
+                pastel
+                nodejs_20
+              ];
+            };
+        }
+      );
+    };
+}
